9dbee6a1d0d2492ff3091dd490b3a2054d752365
Alejandro Silva Quiñones <janosilva@gmail.com>
2024-02-02 01:04:00 -0300

    integracion de footer, mejoras en responsividad y ajustes en JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@
   $(function() {
     $( '#tooltip[title]' ).tooltip();
   });
+
+  //tooltip footer//
+  $('#vaca').on( "mouseenter", function() {
+    $('#hoveringTooltip').remove();
+  });
+  $(function() {
+    $( '#vaca[title]' ).tooltip();
+  });
